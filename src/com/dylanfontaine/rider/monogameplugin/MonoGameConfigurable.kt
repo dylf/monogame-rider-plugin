a3c9e9bae4cfe892f15fc196a34c0afb0c389679
Dylan Fontaine <dylanfontaine@live.ca>
2018-08-22 07:22:43 -0700

    Add settings menu item

---
+++ src/com/dylanfontaine/rider/monogameplugin/MonoGameConfigurable.kt
@@ -0,0 +1,24 @@
+package com.dylanfontaine.rider.monogameplugin
+
+import com.intellij.openapi.options.Configurable
+import javax.swing.JComponent
+
+class MonoGameConfigurable : Configurable {
+
+    override fun getDisplayName(): String {
+        return "MonoGame"
+    }
+
+    override fun apply() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun createComponent(): JComponent? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun isModified(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
