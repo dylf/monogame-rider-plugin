c365c3832ad987d74d050be9eb678e462e72e1dc
Dylan Fontaine <dylanfontaine@live.ca>
2018-08-21 20:09:43 -0700

    Fix some errors

--- src/com/dylanfontaine/rider/monogameplugin/OpenMonoGameContentPipelineAction.kt
+++ src/com/dylanfontaine/rider/monogameplugin/OpenMonoGameContentPipelineAction.kt
@@ -3,23 +3,21 @@ package com.dylanfontaine.rider.monogameplugin
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.ui.Messages
 
 
 class OpenMonoGameContentPipelineAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent?) {
-        Messages.showInfoMessage("Hello, World!", "Hello, World!!!")
         val filePath = e!!.getData(CommonDataKeys.VIRTUAL_FILE)!!.path
-        Runtime.getRuntime().exec(getPipelineCommand() + filePath)
+        Runtime.getRuntime().exec(getPipelineCommand() + " " + filePath)
     }
 
     override fun update(e: AnActionEvent?) {
         val file = e!!.getData(CommonDataKeys.VIRTUAL_FILE)
-        val visible = file != null && file.extension == "mgcbs"
+        val visible = file != null && file.extension == "mgcb"
         e.presentation.setEnabledAndVisible(visible)
     }
 
-    fun getPipelineCommand() : String {
+    private fun getPipelineCommand() : String {
         return "C:\\Program Files (x86)\\MSBuild\\MonoGame\\v3.0\\Tools\\Pipeline.exe"
     }
 }
